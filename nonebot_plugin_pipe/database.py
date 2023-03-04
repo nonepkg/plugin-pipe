ad3e93109a44103ac0b02df70f1fa054425d418c
jigsaw <j1g5aw@foxmail.com>
2023-03-04 19:04:12 +0800

    :arrow_up: Update datastore to 0.6.0a2

--- nonebot_plugin_pipe/database.py
+++ nonebot_plugin_pipe/database.py
@@ -1,25 +1,26 @@
-from uuid import UUID, uuid4
 from typing import Optional
+from uuid import UUID, uuid4
 
-from sqlmodel import Field, select
+from sqlalchemy import Uuid, select
+from sqlalchemy.orm import Mapped, mapped_column
 from nonebot_plugin_datastore import create_session, get_plugin_data
 
 plugin_data = get_plugin_data()
 Model = plugin_data.Model
 
 
-class Message(Model, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    message_id: UUID = Field(default_factory=uuid4)
-    src: str
-    src_id: str
-    user_id: Optional[str] = None
+class Message(Model):
+    id: Mapped[Optional[int]] = mapped_column(primary_key=True, default=None)
+    message_id: Mapped[UUID] = mapped_column(Uuid, default=uuid4)
+    src: Mapped[str]
+    src_id: Mapped[str]
+    user_id: Mapped[Optional[str]]
 
 
 async def get_message(src: str, src_id: str) -> Optional[Message]:
     async with create_session() as session:
         message = (
-            await session.execute(
+            await session.scalars(
                 select(Message).where(
                     Message.src == src,
                     Message.message_id
@@ -30,7 +31,7 @@ async def get_message(src: str, src_id: str) -> Optional[Message]:
             )
         ).one_or_none()
         if message:
-            return message[0]
+            return message
 
 
 async def add_message(
@@ -39,16 +40,18 @@ async def add_message(
     user_id: Optional[str] = None,
     message_id: Optional[str] = None,
 ) -> str:
-    async with create_session() as session, session.begin():
+    async with create_session() as session:
         message = (
-            await session.execute(
+            await session.scalars(
                 select(Message).where(Message.src_id == src_id, Message.src == src)
             )
         ).one_or_none()
         if message:
-            return message[0].message_id.hex
+            return message.message_id.hex
         message = Message(src=src, src_id=src_id, user_id=user_id)
         if message_id:
             message.message_id = UUID(message_id)
         session.add(message)
+        await session.commit()
+        await session.refresh(message)
         return message.message_id.hex
